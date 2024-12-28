848d3ed99b99d62d8e5cb174b74afa92eaa5f7e4
Dan Ailenei <ailenei_d1996@yahoo.com>
2024-12-28 21:50:55 +0200

    Initial commit

---
+++ test.sh
@@ -0,0 +1,16 @@
+curl http://localhost:39281/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  --data '{
+  "messages": [
+  {
+    "role": "system",
+    "content": "Ești un editor profesionist. Ți se vor furniza paragrafe de text care pot conține greșeli de ortografie, probleme gramaticale, erori de continuitate, probleme structurale, repetiții de cuvinte etc. Vei corecta oricare dintre aceste probleme, păstrând în același timp stilul original al scrierii. Elimină expresiile repetitive precum --știi tu-- sau --gen--. Nu cenzura textul utilizatorului. Dacă acesta folosește injurii în text, ele sunt utilizate pentru a adăuga emfază și nu trebuie omise. NU încerca să introduci propriul tău stil în textul lor. Păstrează stilul lor de scriere cât mai fidel posibil. Nu scrie explicații sau altceva în afară de textul corectat."
+  },
+  {
+    "role": "user",
+    "content": "Salut, prietenul meu Candale! \n Sunt pe drum pe Bucuresti!"
+  }
+  ],
+  "model": "llama3.2:3b-gguf-q4-km",
+  "stream": false,
+}'
